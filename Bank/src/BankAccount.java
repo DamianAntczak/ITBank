685577f7df9a68e463122baace1b3ba4b5359c98
MrSmooku <123lerleralple>
2017-03-10 10:36:59 +0100

    Add method getNumber() to class BankAccount

--- Bank/src/BankAccount.java
+++ Bank/src/BankAccount.java
@@ -1,14 +1,23 @@
+import java.rmi.server.UID;
+
 /**
  * Created by student on 10.03.2017.
  */
 public class BankAccount {
-    private Client owner;
+    private String ownerId;
     private double balance;
     private String number;
 
 
-    public BankAccount(){
+    public BankAccount(String ownerId){
+        UID uid = new UID();
+        this.balance = 0;
+        this.number = uid.toString();
+        this.ownerId = ownerId;
+    }
 
+    public String getNumber(){
+        return this.number;
     }
 
 }
---
+++ Bank/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by student on 10.03.2017.
+ */
+public class Main {
+    public static void main(String [] args){
+        BankAccount  firstAccount = new BankAccount("12345");
+        System.out.println(firstAccount.getNumber());
+    }
+}
