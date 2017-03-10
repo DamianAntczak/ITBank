a54a25fe8bd061fa3753160158ce7a0d5704b742
MrSmooku <123lerleralple>
2017-03-10 10:17:02 +0100

    Add class BankAccount , Client

---
+++ Bank/src/BankAccount.java
@@ -0,0 +1,14 @@
+/**
+ * Created by student on 10.03.2017.
+ */
+public class BankAccount {
+    private Client owner;
+    private double balance;
+    private String number;
+
+
+    public BankAccount(){
+
+    }
+
+}
