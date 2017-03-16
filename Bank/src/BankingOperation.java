9bc709bd98962b4e1e0ffb7fed850ffbdc6e6b24
Marcin Karmelita <mkarmelita@hotmail.com>
2017-03-16 17:40:34 +0100

    Changed variable names.

--- Bank/src/BankingOperation.java
+++ Bank/src/BankingOperation.java
@@ -32,11 +32,11 @@ public class BankingOperation {
     public boolean transferOperation(Integer amount, Product fromProduct, Product toProduct) {
         if (fromProduct.getCash(amount)) {
             toProduct.addCash(amount);
-            History.getInstance().addRecord(new RecordForTransfer(this.id, this.date, BankingOperationType.INCOMINGTRANSFER, amount, fromProduct.getNumber().toString(), toProduct.getNumber().toString()));
-            History.getInstance().addRecord(new RecordForTransfer(this.id, this.date, BankingOperationType.OUTCOMINGTRANSFER, amount, toProduct.getNumber().toString(), fromProduct.getNumber().toString()));
+            History.getInstance().addRecord(new RecordForTransfer(this.id, this.date, BankingOperationType.INCOMINGTRANSFER, amount, fromProduct.getId(), toProduct.getId()));
+            History.getInstance().addRecord(new RecordForTransfer(this.id, this.date, BankingOperationType.OUTCOMINGTRANSFER, amount, toProduct.getId(), fromProduct.getId()));
             return true;
         } else {
-            History.getInstance().addRecord(new RecordForTransfer(this.id, this.date, BankingOperationType.REJECTEDTRANSFER, amount, toProduct.getNumber().toString(), fromProduct.getNumber().toString()));
+            History.getInstance().addRecord(new RecordForTransfer(this.id, this.date, BankingOperationType.REJECTEDTRANSFER, amount, toProduct.getId(), fromProduct.getId()));
             return false;
         }
 
@@ -44,14 +44,14 @@ public class BankingOperation {
 
     public void incomingCashOperation(Integer amount, Product toProduct) {
         toProduct.addCash(amount);
-        History.getInstance().addRecord(new RecordForCash(this.id, this.date, BankingOperationType.INCOMINGCASH, amount, "", toProduct.getNumber().toString()));
+        History.getInstance().addRecord(new RecordForCash(this.id, this.date, BankingOperationType.INCOMINGCASH, amount, "", toProduct.getId()));
     }
 
     public void outcomingCashOperation(Integer amount, Product fromProduct) {
         if (fromProduct.getCash(amount)) {
-            History.getInstance().addRecord(new RecordForCash(this.id, this.date, BankingOperationType.OUTCOMINGCASH, amount, fromProduct.getNumber().toString(), ""));
+            History.getInstance().addRecord(new RecordForCash(this.id, this.date, BankingOperationType.OUTCOMINGCASH, amount, fromProduct.getId(), ""));
         } else {
-            History.getInstance().addRecord(new RecordForCash(this.id, this.date, BankingOperationType.REJECTEDCASH, amount, fromProduct.getNumber().toString(), ""));
+            History.getInstance().addRecord(new RecordForCash(this.id, this.date, BankingOperationType.REJECTEDCASH, amount, fromProduct.getId(), ""));
         }
     }
 
