c1c2a252d8d698987b18804d9ab775033c87d6c5
Marcin Karmelita <mkarmelita@hotmail.com>
2017-03-17 10:11:48 +0100

    Added Maven to project and Unit tests

--- Bank/src/RecordInterface.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by marcinkarmelita on 16/03/17.
- */
-public interface RecordInterface {
-    void print();
-}
-
---
+++ Bank/src/test/java/CreditTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by marcinkarmelita on 17/03/17.
+ */
+public class CreditTest {
+
+}
